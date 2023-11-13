56b0e534c68b208d5155266d2990e7261ae6ee69
HAdiiiir <hader.jaamal2010@gmail.com>
2023-11-12 19:48:50 -0800

    a

--- _printf.c
+++ _printf.c
@@ -1,87 +1,78 @@
-#include "main.h"
-#include <stdlib.h>
-
+#include <stdarg.h>
+#include <unistd.h>
+#include "holberton.h"
 /**
- * itoa - Converts an integer to a string
- * @val: Integer to convert
- * @base: Number base for conversion
- *
- * Return: Pointer to the converted string
- */
-char *itoa(int val, int base)
+  * find_function - function that finds formats for _printf
+  * calls the corresponding function.
+  * @format: format (char, string, int, decimal)
+  * Return: NULL or function associated ;
+  */
+int (*find_function(const char *format))(va_list)
 {
-static char buf[32] = {0};
-int i = 30;
+	unsigned int i = 0;
+	code_f find_f[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_dec},
+		{"r", print_rev},
+		{"b", print_bin},
+		{"u", print_unsig},
+		{"o", print_octal},
+		{"x", print_x},
+		{"X", print_X},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
 
-for (; val && i; --i, val /= base)
-buf[i] = "0123456789abcdef"[val % base];
-
-return (&buf[i + 1]);
+	while (find_f[i].sc)
+	{
+		if (find_f[i].sc[0] == (*format))
+			return (find_f[i].f);
+		i++;
+	}
+	return (NULL);
 }
-
 /**
- * _printf - Custom printf function to format and print data
- * @format: format string containing the directives
- *
- * Return: Number of characters printed
- */
+  * _printf - function that produces output according to a format.
+  * @format: format (char, string, int, decimal)
+  * Return: size the output text;
+  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
-char ch;
-char *str;
-int val;
+	va_list ap;
+	int (*f)(va_list);
+	unsigned int i = 0, cprint = 0;
 
-va_start(args, format);
-
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-ch = (char)va_arg(args, int);
-putchar(ch);
-count++;
-break;
-case 's':
-str = va_arg(args, char *);
-while (*str)
-{
-putchar(*str);
-str++;
-count++;
-}
-break;
-case 'd':
-case 'i':
-val = va_arg(args, int);
-str = itoa(val, 10);
-while (*str)
-{
-putchar(*str);
-str++;
-count++;
-}
-break;
-case '%':
-putchar('%');
-count++;
-break;
-}
-}
-else
-{
-putchar(*format);
-count++;
-}
-format++;
-}
-
-va_end(args);
-
-return (count);
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+	while (format[i])
+	{
+		while (format[i] != '%' && format[i])
+		{
+			_putchar(format[i]);
+			cprint++;
+			i++;
+		}
+		if (format[i] == '\0')
+			return (cprint);
+		f = find_function(&format[i + 1]);
+		if (f != NULL)
+		{
+			cprint += f(ap);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		cprint++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(ap);
+	return (cprint);
 }
