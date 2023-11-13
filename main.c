925dcff7ba0c4552a936ade72e178e1324159245
HAdiiiir <hader.jaamal2010@gmail.com>
2023-11-12 20:29:12 -0800

    a

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-_printf("Character: %c\n", 'A');
-_printf("String: %s\n", "Hello, World!");
-_printf("Percent sign: %%\n");
-_printf("Decimal: %d\n", 123);
-_printf("Integer: %i\n", -456);
-
-return (0);
+    _printf("%b\n", 98);
+    return (0);
 }
